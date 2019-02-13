11de65957b419358ea4da7a470901eb441f01423
Remi Momprive <remi.momprive@gmail.com>
2019-02-13 12:28:44 +0100

    Remove Entity interface

--- src/fr/excilys/rmomprive/service/Page.java
+++ src/fr/excilys/rmomprive/service/Page.java
@@ -2,8 +2,6 @@ package fr.excilys.rmomprive.service;
 
 import java.util.List;
 
-import fr.excilys.rmomprive.model.Entity;
-
 public class Page<T> {
 	private List<T> content;
 	private int pageId;
