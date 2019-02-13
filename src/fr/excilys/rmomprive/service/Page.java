38e976faa001abca32848ecd0b1e26150ed30c01
Remi Momprive <remi.momprive@gmail.com>
2019-02-13 09:59:32 +0100

    Fix infinite loop for pages creation

--- src/fr/excilys/rmomprive/service/Page.java
+++ src/fr/excilys/rmomprive/service/Page.java
@@ -2,6 +2,7 @@ package fr.excilys.rmomprive.service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 import fr.excilys.rmomprive.model.Company;
@@ -42,22 +43,23 @@ public class Page {
 	
 	@Override
 	public String toString() {
-		return "Page [content=" + content + ", previous=" + previous + ", next=" + next + "]";
+		return "Page [content=" + content + ", previous=" + (previous != null) + ", next=" + (next != null) + "]";
 	}
 
 	public static List<Page> getPages(List<Entity> entities, int pageSize) {
 		List<Page> pages = new ArrayList<>();
 		
+		
+		Iterator<Entity> iterator = entities.listIterator();
 		// Create pages
-		/// TODO : fix infinite loop
-		while (entities.iterator().hasNext()) {
+		while (iterator.hasNext()) {
 			Page page = new Page();
 	
-			for (int i = 0; i < pageSize; i++) {
-				Entity entity = entities.iterator().next();
-				if (entity != null) {
-					page.getContent().add(entity);
-				}
+			int i = 0;
+			while (iterator.hasNext() && i < pageSize) {
+				Entity entity = iterator.next();
+				page.getContent().add(entity);
+				i++;
 			}
 			
 			pages.add(page);
@@ -70,43 +72,37 @@ public class Page {
 		
 		// Set previous values to pages
 		for (int i = 1; i < pages.size(); i++) {
-			pages.get(i).setNext(pages.get(i - 1));
+			pages.get(i).setPrevious(pages.get(i - 1));
 		}
 
 		return pages;
 	}
 	
+	/**
+	 * This method tests the pagination class
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		List<Entity> companies = Arrays.asList(
 			new Company(1, "a"),
 			new Company(2, "b"),
-			new Company(1, "a"),
-			new Company(2, "b"),
-			new Company(1, "a"),
-			new Company(2, "b"),
-			new Company(1, "a"),
-			new Company(2, "b"),
-			new Company(1, "a"),
-			new Company(2, "b"),
-			new Company(1, "a"),
-			new Company(2, "b"),
-			new Company(1, "a"),
-			new Company(2, "b"),
-			new Company(1, "a"),
-			new Company(2, "b"),
-			new Company(1, "a"),
-			new Company(2, "b"),
-			new Company(1, "a"),
-			new Company(2, "b"),
-			new Company(1, "a"),
-			new Company(2, "b"),
-			new Company(1, "a"),
-			new Company(2, "b")
+			new Company(3, "a"),
+			new Company(4, "b"),
+			new Company(5, "a"),
+			new Company(6, "b"),
+			new Company(7, "a"),
+			new Company(8, "b"),
+			new Company(9, "a"),
+			new Company(10, "b"),
+			new Company(11, "a"),
+			new Company(12, "b")
 		);
 		
-		System.out.println(companies.size());
+		System.out.printf("%d companies\n", companies.size());
 		
 		List<Page> pages = Page.getPages(companies, 5);
 		System.out.println(pages);
+		
+		System.out.printf("%d pages\n", pages.size());
 	}
 }
