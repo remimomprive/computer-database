41445570fcbff95ebac3bb2c3511a35fdf1ce121
Remi Momprive <remi.momprive@gmail.com>
2019-02-14 14:19:05 +0100

    Add some test

---
+++ src/test/java/fr/excilys/rmomprive/ComputerTest.java
@@ -0,0 +1,17 @@
+package fr.excilys.rmomprive;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import fr.excilys.rmomprive.model.Computer;
+import fr.excilys.rmomprive.service.ComputerService;
+
+public class ComputerTest {
+	@Test
+	public void validCreation() {
+		ComputerService computerService = ComputerService.getInstance();
+		Computer computer = new Computer(1, null, null, null, 1);
+		Computer insertedComputer = computerService.add(computer);
+		assertTrue(insertedComputer != null);
+	}
+}
